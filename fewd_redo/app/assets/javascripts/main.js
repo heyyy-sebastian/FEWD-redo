0760c0b3ec63cb4b63df387b8f1be244dacd75f0
mermatriarchy <hailey.miller91@gmail.com>
2017-04-13 18:08:56 -0400

    add failed testing for button removal on submission

--- fewd_redo/app/assets/javascripts/main.js
+++ fewd_redo/app/assets/javascripts/main.js
@@ -1,10 +1,28 @@
 $(document).ready(function() {
 
 	//Hide submit button after click
-	$().click(function(){
+	//$(document).on('click', '#new_student', function(){
+    //	$(this).css('visibility:hidden;')
+	//})
+
+	$('#new_student').submit(function(){
+
+		//if (location.reload(true)) {
+		//localStorage.removeItem(storage);
+		//e.preventDefault();
+		$(this).find(':input[type=submit]').prop('disabled', true);
+		$('#new_student').slideUp().hide();
+		//}
 		//select form
 		//on click, hide form
 		//slide up
 	});
+
+	if (performance.navigation.type == 1) {
+		console.log('page is reloaded');
+		$(document).ready(function(){
+			$('#new_student').slideUp().hide();
+		});
+	}
 
 }); //End jQuery wrapper function
