b08a74521a15f0c7a915bfa0dcf333005904ea4b
mermatriarchy <hailey.miller91@gmail.com>
2017-04-13 19:23:23 -0400

    remove old js notes

--- fewd_redo/app/assets/javascripts/main.js
+++ fewd_redo/app/assets/javascripts/main.js
@@ -1,26 +1,4 @@
 $(document).ready(function() {
-
-	//Hide submit button after click
-	//$(document).on('click', '#new_student', function(){
-    //	$(this).css('visibility:hidden;')
-	//})
-
-	// $('#new_student').submit(function(){
-
-	// 	//if (location.reload(true)) {
-	// 	//localStorage.removeItem(storage);
-	// 	//e.preventDefault();
-	// 	$(this).find(':input[type=submit]').prop('disabled', true);
-	// 	$('#new_student').slideUp().hide();
-	// 	//}
-	// 	//select form
-	// 	//on click, hide form
-	// 	//slide up
-	// });
-
-	// if (performance.navigation.type == 1) {
-	// 	console.log('page is reloaded');
-	// 	$('#new_student').hide();
-	// }
+	//this project might not require any javascript???
 
 }); //End jQuery wrapper function
