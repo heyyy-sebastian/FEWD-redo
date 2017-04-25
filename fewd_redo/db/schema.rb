4347b33cf9770deaf4d81cab1a60c5a454d11e06
mermatriarchy <hailey.miller91@gmail.com>
2017-04-25 13:43:47 -0400

    add foreign key to students table

--- fewd_redo/db/schema.rb
+++ fewd_redo/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406232200) do
+ActiveRecord::Schema.define(version: 20170422170806) do
 
   create_table "students", force: :cascade do |t|
     t.string   "name"
     t.float    "ip_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_students_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
